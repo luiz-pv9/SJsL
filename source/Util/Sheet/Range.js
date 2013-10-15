4c0add14408d887fc3a79db0d07b0eb41ef316b3
Luiz Paulo de Vasconcellos <esin.luiz@gmail.com>
2013-10-15 08:47:08 -0300

    Adding range feature to the Sheet

---
+++ source/Util/Sheet/Range.js
@@ -0,0 +1,17 @@
+;(function(SJsL) {
+
+    SJsL.Range = function(sheet, start, end) {
+
+        this.sheet = sheet;
+        this.start = start;
+        this.end = end;
+    }
+
+    SJsL.Range.prototype.rows = function() {
+    };
+
+    SJsL.Range.prototype.columns = function() {
+    };
+
+
+})(SJsL);
