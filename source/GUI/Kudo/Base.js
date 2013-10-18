19875a005f15ea4cdd8359f5e45322c2694e0995
Luiz Paulo de Vasconcellos <esin.luiz@gmail.com>
2013-10-18 10:59:52 -0300

    Add the GUI / Kudo directive. Not tested (not even runned).

---
+++ source/GUI/Kudo/Base.js
@@ -0,0 +1,48 @@
+SJsLDirectives.directive("kudo", [function() {
+
+	var templateSmile = [
+	].join(" ");
+
+	var templateStar = [
+	].join(" ");
+
+	var templates = {
+		'smile': templateSmile,
+		'star': templateStar
+	};
+
+	return {
+
+		restrict: 'E',
+		scope: {
+
+			afterHover : '=',
+			afterFinish: '=',
+			afterBlur  : '=',
+			hoverTime  : '@'
+		},
+		link: function(scope, elem, attrs) {
+
+			var countdown;
+			elem.hover(function() {
+
+				if(scope.afterHover) { scope.afterHover(); }
+
+				// TODO: Animate
+				countdown = setTimeout(function() {
+
+					if(scope.afterFinish) { scope.afterFinish(); }
+
+				}, scope.hoverTime);
+			});
+
+			elem.blur(function() {
+
+				if(scope.afterBlur) { scope.afterBlur(); }
+				clearTimeout(countdown);
+				// TODO: Reset animation
+			});
+		}
+	}
+
+}]);
